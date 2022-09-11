c5099be217ec80baea96a09c6feda123d111fa18
MITAL SAPKALE <89100054+mitalrs@users.noreply.github.com>
2022-09-11 16:26:49 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (34.92%) - LeetHub

---
+++ 27-remove-element/27-remove-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l=0,r=nums.length-1;
+        
+        while(l<=r)
+        {
+            if(nums[r]==val)
+            {
+                r--;
+                continue;
+            }
+            if(nums[l]==val)
+            {
+                nums[l]=nums[r];
+                r--;
+            }
+            l++;
+        }
+        return l;
+    }
+}
